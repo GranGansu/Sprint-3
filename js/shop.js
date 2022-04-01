0cdd44cf41e51490efb689467f3c57a15f938879
DESKTOP-O393MCR\Gansu <asztamfaterg01@educantabria.es>
2022-04-01 11:16:43 +0200

    Nivel 2

--- js/shop.js
+++ js/shop.js
@@ -65,7 +65,7 @@ var products = [
 ]
 // Array with products (objects) added directly with push(). Products in this array are repeated.
 var cartList = [];
-
+var emptyCart = document.getElementById('emptyCart');
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
 var cart = [];
 
@@ -88,8 +88,12 @@ function cleanCart() {
 function calculateTotal() {
     // Calculate total price of the cart using the "cartList" array
     total = 0;
-    cartList.forEach(element => {
-        total += element.price;
+    cart.forEach(element => {
+        if (element.subTotal && element.subTotal != null) {
+            total += element.subTotal;
+        } else {
+            total += element.price * element.quantity;
+        }
     });
 }
 
@@ -101,7 +105,6 @@ function generateCart() {
     cartList.forEach(element => {
         if (cart.find(elemento => { return elemento == element })) {
             element.quantity++;
-            console.log('sumar')
         } else {
             element.quantity = 1;
             cart.push(element)
@@ -115,9 +118,13 @@ function applyPromotionsCart() {
     cart.forEach(element => {
         if (element.offer && element.quantity >= element.offer.number) {
             element.subTotal = element.offer.unidad * element.quantity;
-            console.log(element.subTotal);
+        }
+        else {
+            element.subTotal = null;
         }
     });
+    calculateTotal();
+    printCart();
 }
 
 
@@ -128,17 +135,53 @@ function addToCart(id) {
     // Refactor previous code in order to simplify it 
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cart array or update its quantity in case it has been added previously.
+    if (productoExistente = cart.find(productoCart => { return productoCart.id == id })) { //Compruebo si existe en el Cart
+        productoExistente.quantity++;
+    } else {
+        let productoNuevo = Object.assign({}, products.find(producto => { return producto.id == id }));
+        productoNuevo.quantity = 1;
+        cart.push(productoNuevo);
+    }
+    applyPromotionsCart();
 }
 
 // Exercise 8
 function removeFromCart(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
+    if (productoExistente = cart.find(productoCart => { return productoCart.id == id })) { //Compruebo si existe en el Cart
+        if (productoExistente.quantity > 1) {
+            productoExistente.quantity--
+        }
+        else {
+            cart = cart.filter(value => { return value.id != id });
+        }
+    }
+    applyPromotionsCart();
 }
 
 // Exercise 9
 function printCart() {
     // Fill the shopping cart modal manipulating the shopping cart dom
+    let numero = 0;
+    let contador = document.getElementById('count_product');
+    let lista = document.querySelector('.list');
+    let totalElement = document.getElementById('total');
+    lista.innerHTML = '';
+    contador.innerHTML = '';
+    cart.forEach(element => {
+        numero += element.quantity;
+        let li = document.createElement('li');
+        lista.appendChild(li);
+        li.innerHTML = `${element.name} $${element.price} x${element.quantity} <button type="button" onclick="removeFromCart(${element.id})" class="btn btn-outline-dark">Borrar</button>`;
+    })
+    totalElement.innerHTML = 'Total: $' + total;
+    contador.innerHTML = numero;
+    if (cart.length != 0) {
+        emptyCart.classList.add('d-none')
+    } else {
+        emptyCart.classList.remove('d-none')
+    }
 }
 
 
